5a69e285b7561951117f8f8ebe557f87a49d90b4
duc do <tvietduc122@gmail.com>
2022-08-01 21:07:48 +0700

    complete link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 import Home from './components/Home';
 import News from './components/News';
 import Contact from './components/Contact';
@@ -7,29 +7,39 @@ import About from './components/About';
 import Route from './components/routes/Route';
 import Link from './components/routes/Link';
 
+// khi click component link -> component appp re-render
+
 function App() {
+
+    const [flagChangePath, setFlagChangePath] = useState(true);
+
+    const handlePathChange = () => {
+        setFlagChangePath(!flagChangePath);
+    }
+
+
     return (
         <div className="App">
             <div>
                 <nav>
                     <ul className="menu container">
                         <li>
-							<Link to="/home">
+							<Link to="/home" onHandleChangePath={handlePathChange}>
 								Home
 							</Link>
                         </li>
                         <li>
-							<Link to="/news">
+							<Link to="/news" onHandleChangePath={handlePathChange}>
 								News
 							</Link>
                         </li>
                         <li>
-							<Link to="/contact">
+							<Link to="/contact" onHandleChangePath={handlePathChange}>
 								Contact
 							</Link>
                         </li>
                         <li>
-							<Link to="/about">
+							<Link to="/about" onHandleChangePath={handlePathChange}>
 								About
 							</Link>
                         </li>
--- src/components/routes/Link.js
+++ src/components/routes/Link.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 function Link(props) {
 
-    const {to, children} = props;
+    const {to, children, onHandleChangePath} = props;
 
     const handleClickChangePath = (event) => {
         event.preventDefault();
         window.history.pushState(null, '', to);
+
+        // excute function parent define from parent
+        onHandleChangePath();
     }
 
     return (
